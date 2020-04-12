7ebf30054fd65582bc2d95b2a668fc8f91077c01
Kwaaac <max.rodriguezidf@gmail.com>
2020-04-12 17:49:52 +0200

    Ajout de fonctionnalité concernant les règles,     -règle aléatoire + numéro de la règle     -règle choisit, en mettant le numéro de la règle     -les règles disponibles     -try catch si il y a une erreur

--- dbmeme.py
+++ dbmeme.py
@@ -14,23 +14,43 @@ async def on_message(message):
 
     channel = message.channel
 
-    if message.content.startswith("!dbdrule"):
-        await channel.send(get_random_rule())
+    try:
+        if message.content.startswith("!dbdbook"):
+            splitedMsg = message.content.split(",")
 
-    elif message.content.startswith("!dbdmeme"):
-        meme = get_random_dbd_meme()
-        await channel.send(meme[0] + "\n" + meme[1])
+            if len(splitedMsg) > 1:
+                if splitedMsg[1] == "count":
+                    msg = "Voici les règles actuellement disponible au sein du livre:\n"
+                    for i, rule in enumerate(get_nbr_rules()):
+                        if i != len(get_nbr_rules()) - 1:
+                            msg += rule + ", "
+                        else:
+                            msg += rule + "."
 
-    elif message.content.find("pigeon") != -1 or message.content.find("pigeons") != -1:
-        msg = get_random_pigeon() + client.get_user(166557610948952065).mention
-        await channel.send(msg)
+                    await channel.send(msg)
+                    return
+
+                rule = (splitedMsg[1], get_rule(splitedMsg[1]))
+
+            else:
+                rule = choice(list(get_random_rule()))
+
+            await channel.send(
+                "**Règle numéro " + rule[0] + " du livre de règle des tueurs à l'égard des survivants:** \n" +
+                "*" + rule[1] + "*")
+
+        elif message.content.startswith("!dbdmeme"):
+            meme = get_random_dbd_meme()
+            await channel.send(meme[0] + "\n" + meme[1])
+
+        elif message.content.find("pigeon") != -1 or message.content.find("pigeons") != -1:
+            msg = get_random_pigeon() + client.get_user(166557610948952065).mention
+            await channel.send(msg)
+
+    except Exception as e:
+        print("aie y'a eu une bille: \n", e)
 
 
-# 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map',
-# 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric',
-# 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition',
-# 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith',
-# 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
 @client.event
 async def on_ready():
     print('Logged in as')
--- jsonSentencesHandler.py
+++ jsonSentencesHandler.py
@@ -2,15 +2,26 @@ import json
 from random import choice
 
 
-def get_json(part=None):
+def get_json(part=None, rule=None):
     with open("sentences.json", "r", encoding='utf-8') as jsonFile:
         if part is not None:
+            if rule is not None:
+                return json.loads(jsonFile.read())[part][rule]
+
             return json.loads(jsonFile.read())[part]
         return json.loads(jsonFile.read())
 
 
 def get_random_rule():
-    return choice(get_json('book'))
+    return get_json('book').items()
+
+
+def get_rule(rule):
+    return get_json('book', rule)
+
+
+def get_nbr_rules():
+    return get_json('book').keys()
 
 
 def get_random_pigeon():
